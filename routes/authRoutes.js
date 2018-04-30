32176f3f392cba0f08fb04ccf7958ca70ecd8ded
Jens Kai <jkaih7@gmail.com>
2018-04-30 19:16:28 -0400

    made congrats page

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,15 +1,43 @@
 const express = require('express');
-const router  = express.Router();
+const authRouter  = express.Router();//const name must be same everywhere
+
+//== user model to handle sign up request
+const User = require("../models/user");
+
+//== bcrypt to encrypt passwords
+const bcrypt = require('bcrypt');
+const bcryptSalt = 10;
+
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+authRouter.get('/', (req, res, next) => {
   res.render('index');
 });
 
 /* signup page */
-router.get('/signup', (req, res, next) => {
+authRouter.get('/signup', (req, res, next) => {
     res.render('auth/signup');
 })
 
+/* signup post */
+authRouter.post('/signup', (req, res, next) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-module.exports = router;
+    const newUser = User({
+        username: username,
+        password: hashPass,
+    });
+    newUser.save((err) => {
+        res.redirect('/');
+    });
+});
+
+/* congrats page after sign up */
+authRouter.get('/congrats', (req, res, next) => {
+    res.render('auth/congrats')
+})
+
+module.exports = authRouter;
